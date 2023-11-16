90977d2b07064b798011d14f67f27c78431e12d0
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-11-16 16:33:19 +0100

    fixed

--- src/MailProvider.php
+++ src/MailProvider.php
@@ -15,14 +15,8 @@ class MailProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-mail');
-
         $this->publishes([
             __DIR__.'/../database/migrations' => database_path('/migrations')
         ], 'laravel-mail-migrations');
-
-        $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('laravel-mail.php')
-        ], 'laravel-mail-config');
     }
 }
