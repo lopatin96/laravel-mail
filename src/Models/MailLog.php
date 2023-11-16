71486b1bf40af54567ae03d805c9bb7256636465
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-11-16 16:20:44 +0100

    HasNovaBadges

--- src/Models/MailLog.php
+++ src/Models/MailLog.php
@@ -5,10 +5,11 @@ namespace Atin\LaravelMail\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Atin\LaravelNovaBadges\Traits\HasNovaBadges;
 
 class MailLog extends Model
 {
-    use HasFactory;
+    use HasNovaBadges, HasFactory;
 
     protected $fillable = [
         'user_id',
